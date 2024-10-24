cc5e9483e00deed4b4e69ceea140b64bd6951ce8
EmmaIP <emmapineiro@gmail.com>
2024-10-24 15:09:53 +0200

    ejercicio 28 finalizado

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -1,7 +1,15 @@
 package com.campusdual.classroom;
 
 public class Exercise28 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
+        int number1 = 6;
+        int number2 = 0;
+        count(number1, number2);
 
     }
+    public static int count(int number, int number1) {
+       return number/number1;
+    }
+
 }
